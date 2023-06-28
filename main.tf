4435567363f9f7d8cc5a5e715d261ca61b1b45ad
João Victor da Silva <jvictor200212@gmail.com>
2023-06-27 22:23:30 -0300

    feat: adicionado resource de schema do topico, adicionado arquivo do schema em json, adicionado arquivos terraform para variaveis e refatorado o codigo para as variaveis

--- main.tf
+++ main.tf
@@ -8,16 +8,27 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("projeto_gcp_credentials.json")
+  credentials = file(var.credentials)
 
-  project = "airy-charmer-386522"
+  project = var.project
   region  = "us-central1"
   zone    = "us-central1-c"
+}
+
+resource "google_pubsub_schema" "topic_schemas" {
+  name = "terraform-topic-schema"
+  type = "AVRO"
+  definition = file(var.topic_schema)
 }
 
 resource "google_pubsub_topic" "topic_test" {
   name = "terraform-topic-test"
 
+  depends_on = [google_pubsub_schema.topic_schemas]
+  schema_settings {
+    schema = "projects/${var.project}/schemas/${google_pubsub_schema.topic_schemas.name}"
+    encoding = "JSON"
+  }
   labels = {
     category = "terraform"
   }
@@ -27,6 +38,7 @@ resource "google_pubsub_subscription" "subscription_test" {
   name = "${google_pubsub_topic.topic_test.name}-sub"
   topic = google_pubsub_topic.topic_test.name
 
+  depends_on = [google_pubsub_topic.topic_test]
   labels = {
     category = "terraform"
   }
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "project" {
+  default = "airy-charmer-386522"
+}
+
+variable "credentials" {
+  default = "projeto_gcp_credentials.json"
+}
+
+variable "topic_schema" {
+  default = "schema.json"
+}
